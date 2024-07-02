8814d7677a94ca303d619b029745c89e936e266d
Barry Mays <barrykmays@gmail.com>
2024-07-02 19:24:15 +0000

    Add function to display and process hand winner

--- run.py
+++ run.py
@@ -18,8 +18,6 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('top_trumps')
 
-##player_cards = []
-##computer_cards = []
 
 def play_or_edit():
     """
@@ -29,25 +27,27 @@ def play_or_edit():
     choice = ""
     valid = {'p', 'd', 'i'}
     while choice.lower() not in valid:
-        choice = input("P - Play a game of Top Trumps\nD - Edit / View the game data\nI - Instructions\n")
+        choice = input("P - Play a game of Top Trumps\nD - Edit / View the \
+game data\nI - Instructions\n")
         if choice.lower() in valid:
             return choice.lower()
         else:
             print(f"Invalid input, please choose either 'P', 'D' or 'I'")
 
-            
+
 def get_decks():
     """
     Pull the list of worksheet names from the workbook.
     Displays them to the user as a list of available game decks.
     Returns a list of game decks.
-    """    
+    """
     decks = []
     print(f'Available decks:\n')
     for sh in SHEET.worksheets():
         decks.append(sh.title)
         print(f'{sh.title}')
     return decks
+
 
 def choose_deck(decks):
     """
@@ -63,7 +63,7 @@ def choose_deck(decks):
 
 def get_cards(deck):
     """
-    Get the deck data from the chosen google sheet. 
+    Get the deck data from the chosen google sheet.
     First row is the catgories.
     Second row provides winning criteria for each category (high / low)
     Returns a list of categories, criteria and cards.
@@ -74,64 +74,72 @@ def get_cards(deck):
     print(criteria)
     return categories, criteria, cards
 
+
 def shuffle_and_deal(cards):
     """
-    Takes a deck of cards in a list, shuffles them and deals them to two players.
-    If the number of cards in the deck is odd, the last card is removed after the shuffle.
-    2 lists (hands) of cards are returned, one for the player and one for the computer
+    Takes a deck of cards in a list, shuffles them and deals them to two
+    players. If the number of cards in the deck is odd, the last card is
+    removed after the shuffle. 2 lists (hands) of cards are returned, one for
+    the player and one for the computer.
     """
     hand_one = []
     hand_two = []
     shuffle(cards)
-    
+
     if len(cards) % 2 != 0:
         cards.pop()
-    
-    for i in range(0,len(cards),2):
+
+    for i in range(0, len(cards), 2):
         hand_one.append(cards[i])
         hand_two.append(cards[i+1])
 
     return hand_one, hand_two
 
+
 def display_game_state(num1, num2):
     """
-    Takes two numbers and displays them in a table, showing the number of cards remaining
-    for each player
+    Takes two numbers and displays them in a table, showing the number of cards
+    remaining for each player.
     """
     table = PrettyTable()
     table.field_names = ["Player", "Cards Remaining"]
-    table.add_rows ([
+    table.add_rows([
         ["Player", num1],
-        ["Computer", num2] 
+        ["Computer", num2]
     ])
-    
+
     print(table)
+
 
 def display_next_card(categories, card):
     """
-    Takes two lists as parameters of equal length and displays them in columns  input Shows the next card in the player's hand to be played.
+    Takes two lists as parameters of equal length and displays them in columns.
+    Shows the player their next card to be played.
     """
     table = PrettyTable()
-    table.add_column("#", ["",1,2,3,4,5,6])
+    table.add_column("#", ["", 1, 2, 3, 4, 5, 6])
     table.add_column("Category", categories)
     table.add_column("Player Card", card)
-    print (table)
+    print(table)
+
 
 def player_choose_category():
     """
-    Asks the user to input a numer 1-6 corresponding to the displayed categories.
-    Returns an integer 1-6
+    Asks the user to input a numer 1-6 corresponding to the displayed
+    categories. Returns an integer 1-6.
     """
     try:
         cat = int(input('Player, choose the category to play (1-6)\n'))
         if cat > 0 and cat < 7:
             return cat
-        else: 
-            print("Invalid input. Must be a number between 1 and 6, try again\n")
+        else:
+            print("Invalid input. Must be an integer 1-6, try again.\n")
             player_choose_category()
     except (ValueError):
-            print("That's not even an integer, it must be an integer between 1 and 6, try again..\n")
-            player_choose_category()
+        print("That's not even an integer, it must be a number\
+        1-6, try again...\n")
+        player_choose_category()
+
 
 def computer_choose_category():
     """
@@ -141,84 +149,123 @@ def computer_choose_category():
     cat = random.randrange(6) + 1
     return cat
 
+
 def create_column(row):
     """
-    Takes an integer 1-6 as a parameter and creates a list, which is used next to the displayed
-    cards showing which category was chosen for this comparison.
+    Takes an integer 1-6 as a parameter and creates a list, which is displayed
+    next to the displayed cards showing which category was chosen for this
+    comparison.
     """
-    column=[""]
-    for i in range(1,7):
+    column = [""]
+    for i in range(1, 7):
         if i == row:
             column.append("<<<<< ?? >>>>>")
         else:
             column.append("")
     return column
 
+
 def display_both_cards(categories, card_p, card_c, selected):
     """
     Takes 4 lists and displays them all as columns
     Displays the computer card to the user as a column in the table.
-    Returns the table 
+    Returns the table.
     """
     table = PrettyTable()
-    table.add_column("#", ["",1,2,3,4,5,6])
+    table.add_column("#", ["", 1, 2, 3, 4, 5, 6])
     table.add_column("Category", categories)
     table.add_column("Player Card", card_p)
     table.add_column("Chosen Category", selected)
     table.add_column("Computer Card", card_c)
-    print (table)
+    print(table)
     input("Press any key to confirm the winner")
     return table
 
+
 def get_winner(criteria, play, comp):
     """
-    Takes the player and computer values for selected category, and wining criteria
-    as parameters category. Compares the values and returns the winning card 
-    p (player), c (computer) or t(tie)
+    Takes the player and computer values for selected category, and winning
+    criteria as parameters category. Compares the values and returns the
+    result: p (player), c (computer) or t (tie)
     """
-    
     criteria = criteria.lower()
-    print(criteria, play, comp)
-
-    if play > comp:
+    
+    if int(play) > int(comp):
         if criteria == "h":
             return "p"
         else:
             return "c"
-    elif play < comp:
+    elif int(play) < int(comp):
         if criteria == "h":
             return "c"
         else:
             return "p"
     else:
         return "t"
-        
+
+
+def process_winner(winner, p_hand, c_hand, held):
+    """
+    Declares the winner to the terminal and adds the played cards to the bottom
+    of their pile. In the event of a tie, the cards are placed into the hold
+    list.
+    """
+    c_card = c_hand.pop(0)
+    p_card = p_hand.pop(0)
+
+    match winner:
+        case 'c':
+            print(f'Computer wins!\n')
+            c_hand.append(c_card)
+            c_hand.append(p_card)
+            print(f'c:{c_hand}')
+            print(f'p:{p_hand}')
+            for i in range(0, len(held)):
+                c_hand.append(held[i])
+            held=[]
+        case 'p':
+            print(f'Player wins!\n')
+            p_hand.append(p_card)
+            p_hand.append(c_card)
+            print(f'c:{c_hand}')
+            print(f'p:{p_hand}')
+            for i in range(0, len(held)):
+                p_hand.append(held[i])
+            held=[]
+        case 't':
+            print(f"It's a tie! Cards are held for the next winner.\n")
+            held.append(p_card)
+            held.append(c_card)
+            
 
 def run_game(deck):
     """
     Runs a game of top trumps with the chosen deck
     """
-    player_turn = False 
+    player_turn = False
     categories, criteria, all_cards = get_cards(deck)
-    ##print(all_cards)
+    # print(all_cards)
     player_cards, computer_cards = shuffle_and_deal(all_cards)
-    ##print(categories)
-    ##print(f'P1: {player_cards}')
-    ##print(f'P2: {computer_cards}')
+    held_cards = []
     display_game_state(len(player_cards), len(computer_cards))
     display_next_card(categories, player_cards[0])
-    if player_turn == True:
+    if player_turn is True:
         chosen_category = player_choose_category()
-        print(f'You have chosen to play the category: {categories[chosen_category]}\n')
+        print(f'You have chosen to play the category: \
+        {categories[chosen_category]}\n')
     else:
         chosen_category = computer_choose_category()
-        print(f'The computer has chosen to play the category: {categories[chosen_category]}\n')
+        print(f'The computer has chosen to play the category: \
+        {categories[chosen_category]}\n')
 
     cat_column = create_column(chosen_category)
-    show = display_both_cards (categories, player_cards[0], computer_cards[0], cat_column)
+    show = display_both_cards(categories, player_cards[0], computer_cards[0],
+           cat_column)
     num = chosen_category
-    winner = get_winner(criteria[num], player_cards[0][num], computer_cards[0][num])
-    print (winner)
+    winner = get_winner(criteria[num], player_cards[0][num],
+             computer_cards[0][num])
+    process_winner(winner, player_cards, computer_cards, held_cards)
+
 
 def main():
     """
@@ -227,9 +274,10 @@ def main():
     option = play_or_edit()
     all_decks = get_decks()
     chosen_deck = choose_deck(all_decks)
-    
+
     if option == 'p':
         print(f'Chosen deck = {chosen_deck}')
         run_game(chosen_deck)
-        
+
+
 main()
